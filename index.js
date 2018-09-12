303df11dacd43631196a8bacdf97084ab00115f4
Max Riepkin <ryepkin.maksym98@gmail.com>
2018-09-12 13:29:50 +0300

    updated platform name(can be lowercase now)

--- index.js
+++ index.js
@@ -59,53 +59,12 @@ function getNewToken(oAuth2Client, callback) {
             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
                 if (err) console.error(err);
                 console.log('Token stored to', TOKEN_PATH);
-                process.exit();
+                process.exit(1);
             });
             callback(oAuth2Client);
         });
     });
 }
-
-
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function listMajors(auth) {
-    const sheets = google.sheets({version: 'v4', auth});
-    sheets.spreadsheets.values.get({
-        spreadsheetId: '1iGmKHDSYiIzuw5M4h88boy-UTsT_2IPT08AiCAAtdj4',
-        range: 'Strings!D4:D27',
-
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const rows = res.data.values;
-        if (rows.length) {
-            console.log('Name, Major:');
-            // Print columns A and E, which correspond to indices 0 and 4.
-            rows.map((row) => {
-                console.log(`${row[0]}`);
-            });
-        } else {
-            console.log('No data found.');
-        }
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -303,7 +262,7 @@ async function getNeededData(auth) {
     }catch (e) {
         console.log('SCRIPT EXECUTION FAILED');
         console.log(e);
-        process.exit();
+        process.exit(1);
     }
 }
 
